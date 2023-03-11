dc383befd5be6946b4c6a7f568cb9b4df36018ae
Alvaro Gutierrez Gonzalez <alvaro3639@gmail.com>
2023-03-11 23:57:46 +0100

    fix wait_chaos_finish

--- scripts/wait_chaos_finish.sh
+++ scripts/wait_chaos_finish.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-until ./kubectl get workflow  --sort-by=.metadata.creationTimestamp -o jsonpath='{.items[-1:].metadata.labels.\\workflows\\.argoproj\.io\\/phase}' -nlitmus | grep -m 1 "Succeeded\|Failed";
+echo "Waiting for the chaos experiment to finish...";
+
+until ./kubectl get workflow  --sort-by=.metadata.creationTimestamp -o jsonpath='{.items[-1:].metadata.labels.\workflows\.argoproj\.io\/phase}' -nlitmus | grep -m 1 "Succeeded\|Failed";
 do
-  echo "Waiting for the chaos experiment to finish...";
   sleep 5;
 done
+
+echo "Chaos experiment finished."
